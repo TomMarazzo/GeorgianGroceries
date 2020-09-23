52116a35ff671d7fa849422f8f0b19935b8005b7
TomMarazzo <camenm@hotmail.com>
2020-09-22 23:15:13 -0400

    Added moch data

--- Controllers/StoreController.cs
+++ Controllers/StoreController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GeorgianGroceries.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeorgianGroceries.Controllers
@@ -10,11 +11,20 @@ namespace GeorgianGroceries.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //Use fake Category class/model to create and display 10 categories
+            //1. Create an Object to hold a list of categories
+            var categories = new List<Category>();
+            for (var i = 1; i <= 10; i++)
+            {
+                categories.Add(new Category { Id = i, Name = "Category " + i.ToString() });
+            }
+            //modify the return View so that it now accepts a list of categories to pass to the view for display
+            return View(categories);
         }
 
-        public IActionResult Browse()
+        public IActionResult Browse(string category)
         {
+            ViewBag.category = category;
             return View();
         }
     }
