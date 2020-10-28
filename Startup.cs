4607acbc80084dc1c37f9f917bff776602abd413
TomMarazzo <camenm@hotmail.com>
2020-10-28 08:35:10 -0400

    Add Google Auth and OAuth Funtionality

--- Startup.cs
+++ Startup.cs
@@ -33,6 +33,14 @@ namespace GeorgianGroceries
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            //Enable Google Auth using the API keys we created in Google Dev Console
+            services.AddAuthentication()
+                .AddGoogle(option =>
+                {
+                    option.ClientId = Configuration.GetSection("Authentication:Google")["ClientId"];
+                    option.ClientSecret = Configuration.GetSection("Authentication: Google")["ClientSecret"];
+                });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
