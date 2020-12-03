91296e5b53ea97dddd88b4449234492cc1d05aac
TomMarazzo <camenm@hotmail.com>
2020-12-02 19:32:12 -0500

    Update to DummiesControllertest.cs

---
+++ GeorgianGroceriesTests/DummiesControllerTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeorgianGroceriesTests
+{
+    [TestClass]
+    public class DummiesControllerTest
+    {
+        [TestMethod]
+        public void IndexReturnsSomething()
+        {
+            // arrange - set up any variables / objects needed for the method & scenario we want to test
+
+            // act - execute the method and store the result returned (if any)
+
+            // assert - evaluate if the result is as expected
+        }
+    }
+}
